903a0f86cd48a862a1702d49b947d34934a9d2df
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2025-08-18 15:30:28 +0700

    update env

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,16 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-if (!import.meta.env.VITE_SUPABASE_URL) {
-  throw new Error('Missing environment variable: VITE_SUPABASE_URL');
-}
-
-if (!import.meta.env.VITE_SUPABASE_KEY) {
-  throw new Error('Missing environment variable: VITE_SUPABASE_KEY');
-}
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
